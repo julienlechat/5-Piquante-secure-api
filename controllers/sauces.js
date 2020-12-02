b41413cb39b0ead856936991675041a721d2e683
Julien LECHAT <3fight@free.fr>
2020-12-02 15:42:02 +0100

    Amélioration des functions dans le controllers Sauces

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -32,15 +32,17 @@ exports.postSauce = (req, res, next) => {
 
 // MODIFIE UNE SAUCE
 exports.editSauce = (req, res, next) => {
+
+    // On récupére l'id de la sauce
     const sauceId = {_id: req.params.id}
-
-    function updateSauce() {
-        function bodyContentImg() {
-            if (!req.file) return {...req.body}
-            return {...JSON.parse(req.body.sauce),imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`}
-        }
-
-        Sauce.updateOne(sauceId, {...bodyContentImg(), _id: req.params.id})
+    // On défini le contenue suivant le cas ou l'utilisateur envoie une image ou non
+    const content = () => {
+        if (!req.file) return {...req.body}
+        return {...JSON.parse(req.body.sauce),imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`}
+    }
+    // On lance la fonction update
+    const updateSauce = () => {
+        Sauce.updateOne(sauceId, {...content(), _id: req.params.id})
             .then(() => res.status(200).json({message: 'Objet modifié !'}))
             .catch(error => res.status(400).json({error}))
     }
@@ -49,7 +51,7 @@ exports.editSauce = (req, res, next) => {
         Sauce.findOne(sauceId)
             .then(img => {
                 const filename = img.imageUrl.split('/images/')[1]
-                fs.unlink(`images/${filename}`, () => {return updateSauce()})
+                fs.unlink(`images/${filename}`, () => {updateSauce()})
             })
             .catch(error => res.status(500).json({error}))
     } else {updateSauce()}
@@ -66,21 +68,20 @@ exports.deleteSauce = (req, res, next) => {
 exports.likeSauce = (req, res, next) => {
     var sauceId = {_id: req.params.id}
     
-    function applyLike(status) {
-
-         function editFn(st) {
-             console.log(st, req.body.like)
-            // SI JE NE SUIS PAS DANS UN TABLEAU
-            if (st === 0 && req.body.like === 1) return {$inc: {likes: 1},$push: {usersLiked: req.body.userId}, _id: req.params.id}
-            if (st === 0 && req.body.like === -1) return {$inc: {dislikes: 1},$push: {usersDisliked: req.body.userId}, _id: req.params.id}
-            // SI JE SUIS DANS LE TABLEAU LIKE
-            if (st === 1 && req.body.like === 0) return {$inc: {likes: -1},$pull: {usersLiked: req.body.userId}, _id: req.params.id}
-            if (st === 1 && req.body.like === -1) return {$inc: {likes: -1, dislikes: 1},$pull: {usersLiked: req.body.userId},$push: {usersDisliked: req.body.userId}, _id: req.params.id}
-            // SI JE SUIS DANS LE TABLEAU DISLIKE
-            if (st === 2 && req.body.like === 1) return {$inc: {likes: 1, dislikes: -1},$pull: {usersDisliked: req.body.userId},$push: {usersLiked: req.body.userId}, _id: req.params.id}
-            if (st === 2 && req.body.like === 0) return {$inc: {dislikes: -1},$pull: {usersDisliked: req.body.userId}, _id: req.params.id}
-        }
-
+    // Défini les changements par rapport au status | st 0 -> pas de like/dislike | st 1 -> utilisateur présent dans tableau des likes | st 2 -> utilisateur dans tableau dislike
+    const editFn = (st) => {
+           // SI JE NE SUIS PAS DANS UN TABLEAU
+           if (st === 0 && req.body.like === 1) return {$inc: {likes: 1},$push: {usersLiked: req.body.userId}, _id: req.params.id}
+           if (st === 0 && req.body.like === -1) return {$inc: {dislikes: 1},$push: {usersDisliked: req.body.userId}, _id: req.params.id}
+           // SI JE SUIS DANS LE TABLEAU LIKE
+           if (st === 1 && req.body.like === 0) return {$inc: {likes: -1},$pull: {usersLiked: req.body.userId}, _id: req.params.id}
+           if (st === 1 && req.body.like === -1) return {$inc: {likes: -1, dislikes: 1},$pull: {usersLiked: req.body.userId},$push: {usersDisliked: req.body.userId}, _id: req.params.id}
+           // SI JE SUIS DANS LE TABLEAU DISLIKE
+           if (st === 2 && req.body.like === 1) return {$inc: {likes: 1, dislikes: -1},$pull: {usersDisliked: req.body.userId},$push: {usersLiked: req.body.userId}, _id: req.params.id}
+           if (st === 2 && req.body.like === 0) return {$inc: {dislikes: -1},$pull: {usersDisliked: req.body.userId}, _id: req.params.id}
+    }
+    // Met à jour
+    const applyLike = (status) => {
         Sauce.updateOne(sauceId, editFn(status))
                 .then(() => res.status(200).json({message: 'Like publié !'}))
                 .catch(error => res.status(400).json({error}))
@@ -88,7 +89,6 @@ exports.likeSauce = (req, res, next) => {
 
     Sauce.findOne(sauceId)
         .then(sauce => {
-            console.log(sauce)
             if (sauce.usersLiked.find((user) => user === req.body.userId)) return applyLike(1)
             if (sauce.usersDisliked.find((user) => user === req.body.userId)) return applyLike(2)
             return applyLike(0)
--- middleware/auth.js
+++ middleware/auth.js
@@ -6,12 +6,13 @@ module.exports = (req, res, next) => {
         const token = req.headers.authorization.split(' ')[1]
         const decodeToken = jwt.verify(token, 'W0TFH87VH8NgAINL-EQrXbaBZ-A0i2lrnENcv6zzqsz70QnJ2vQOfif3RaUp2Py9lBRpVTsmnkGuawKGHJ6dbLSvIqoAJKo2V2X4oACal0')
         const userId = decodeToken.userId
+
         if (req.body.userId && req.body.userId !== userId) {
-            throw 'userID non valide!'
+            throw 'userID non valide'
         } else {
             next()
         }
     } catch {
-        res.status(401).json({error: new Error('Invalid request!')})
+        res.status(401).json({error: new Error('Requête non valide!')})
     }
 }
